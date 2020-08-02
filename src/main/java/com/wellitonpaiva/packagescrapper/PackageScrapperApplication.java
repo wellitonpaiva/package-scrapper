f225b53fe4678748dd411f0f1ed2e67852f3b756
Welliton Paiva <wellitonpaiva@gmail.com>
2020-08-02 20:43:52 +0100

    Initial commit

---
+++ src/main/java/com/wellitonpaiva/packagescrapper/PackageScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.wellitonpaiva.packagescrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellitonpaiva/packagescrapper/PackageScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellitonpaiva.packagescrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
